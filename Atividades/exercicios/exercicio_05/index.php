0f2de7d3dc3cb6edc8d0117b7cc5a41989cf2de9
Helena Brito Dias <helenabdias95@gmail.com>
2022-04-30 11:25:45 -0300

    exercicio 05

---
+++ Atividades/exercicios/exercicio_05/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Exercicio 05</title>
+    <link rel="stylesheet" href="../css/style.css">
+
+</head>
+
+<body>
+
+    <?php
+    $nome = "João";
+    $idade = 20;
+    $sexo = "Masculino";
+    echo "<h1>Dados do Usuário</h1>";
+    echo "<h3>Nome: </h3><p>$nome</p>";
+    echo "<h3>Idade: </h3><p>$idade</p>";
+    echo "<h3>Sexo: </h3><p>$sexo</p>";
+    ?>
+
+</body>
+
+</html>
